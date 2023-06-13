fbe9dee16ecd014021ab4aac3665548b79d37d45
sagar sangwan <iamsagarsangwan@gmail.com>
2023-06-13 12:31:18 +0530

    updated urls.py

--- articles/urls.py
+++ articles/urls.py
@@ -1,12 +1,14 @@
 
 from django.urls import path, include
-from .views import ArticleViewSet
+from .views import ArticleViewSet, newArticleViewSet
 from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
 router.register('', ArticleViewSet, basename='articles')
+router.register('/<int:pk>/', ArticleViewSet, basename='articles')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
