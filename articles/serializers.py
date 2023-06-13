2f04b1a3021840e28794cbac7a0945469a2c990a
sagar sangwan <iamsagarsangwan@gmail.com>
2023-06-13 17:23:48 +0530

    done backend

--- articles/serializers.py
+++ articles/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Article
 from django.contrib.auth.models import User
 
+from rest_framework.authtoken.models import Token
+
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,3 +15,15 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password']
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'required': True
+            }
+
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        token = Token.objects.create(user=user)
+        return user
